b98b2f5641ee0d68b49e3a8fa7dd63111d884d87
Adam Newman <anewman1@ku.edu>
2019-10-26 11:06:08 -0500

    all the code, all of it

---
+++ client/build/precache-manifest.d8e3705b6cb3d2a04fc1e2f991f00056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403111da17a4105bdb21d118573a57de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "326f1dfde1ff6d4893c5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "55838d685c2c5d53b886",
+    "url": "/static/js/2.eb87e4ff.chunk.js"
+  },
+  {
+    "revision": "326f1dfde1ff6d4893c5",
+    "url": "/static/js/main.95cdfb52.chunk.js"
+  },
+  {
+    "revision": "32c387f3e7d2316ac4d4",
+    "url": "/static/js/runtime-main.fd8d8965.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
