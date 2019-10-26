8e05cedb1fb390b80345eb2b0453efa8e2c09d95
Adam Newman <anewman1@ku.edu>
2019-10-26 12:46:07 -0500

    make it better

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import SaveBooks from "./pages/SaveBooks";
 import SearchBooks from "./pages/SearchBooks";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
-import Footer from "./components/Footer"
-import "./App.css"
 
+import "./App.css"
 
 function App() {
   return (
@@ -19,7 +19,6 @@ function App() {
           <Route exact path="/saved/:id" component={SaveBooks} />
           <Route component={NoMatch} /> 
         </Switch>
-        <Footer />
       </div>
     </Router>
   );
